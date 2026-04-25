c02c933b0cd631a2c04eabfb9902985c95e04876
Abuzar Younas Malik G <abuzaryounasmalikg@gmail.com>
2026-04-25 11:38:33 +0500

    Update Calculator.cpp

--- Calculator.cpp
+++ Calculator.cpp
@@ -5,40 +5,54 @@ using namespace std;
 int main() {
     char operation;
     double num1, num2;
+    bool keepRunning = true; // Program ko chalte rakhne ke liye
 
-    cout << "--- Simple C++ Calculator ---" << endl;
-    cout << "Enter operator (+, -, *, /): ";
-    cin >> operation;
+    cout << "--- My Simple C++ Calculator ---" << endl;
 
-    cout << "Enter two numbers: ";
-    cin >> num1 >> num2;
+    while (keepRunning) {
+        // 1. Operator input lein
+        cout << "\nEnter operator (+, -, *, /) or 'x' to exit: ";
+        cin >> operation;
 
-    switch(operation) {
-        case '+':
-            cout << num1 << " + " << num2 << " = " << num1 + num2;
-            break;
+        // Agar user 'x' dabaye to program band kar dein
+        if (operation == 'x') {
+            keepRunning = false;
+            continue; 
+        }
 
-        case '-':
-            cout << num1 << " - " << num2 << " = " << num1 - num2;
-            break;
+        // 2. Numbers input lein
+        cout << "Enter two numbers: ";
+        cin >> num1 >> num2;
 
-        case '*':
-            cout << num1 << " * " << num2 << " = " << num1 * num2;
-            break;
+        // 3. Calculation logic
+        switch(operation) {
+            case '+':
+                cout << "Result: " << num1 + num2 << endl;
+                break;
 
-        case '/':
-            if(num2 != 0)
-                cout << num1 << " / " << num2 << " = " << num1 / num2;
-            else
-                cout << "Error! Division by zero is not possible.";
-            break;
+            case '-':
+                cout << "Result: " << num1 - num2 << endl;
+                break;
 
-        default:
-            // Agar user galat operator enter kare
-            cout << "Error! Operator is not correct";
-            break;
+            case '*':
+                cout << "Result: " << num1 * num2 << endl;
+                break;
+
+            case '/':
+                if(num2 != 0)
+                    cout << "Result: " << num1 / num2 << endl;
+                else
+                    cout << "Error! Division by zero is not possible." << endl;
+                break;
+
+            default:
+                cout << "Error! Invalid operator. Please try again." << endl;
+                break;
+        }
+        
+        cout << "-----------------------------" << endl;
     }
 
-    cout << endl << "-----------------------------" << endl;
+    cout << "Calculator closed. Good Buy!" << endl;
     return 0;
 }
